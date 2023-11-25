4bd9aef1ea10be3c7fd19a6b0ff4d0d13fd27444
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-26 03:03:33 +0600

    Add getUsers controller for retrieving users

---
+++ src/api/user/controllers/getUsers.js
@@ -0,0 +1,15 @@
+const User = require("../../../models/User");
+
+// Get users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(503).send({message: "Users Unavailable"});
+  }
+};
+
+
+module.exports = getUsers;
