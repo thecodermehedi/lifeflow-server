57e3c05680117b445cfc446e4dd38459496842b4
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-26 03:02:59 +0600

    Add removeAuthCookie controller

---
+++ src/api/auth/controllers/removeAuthCookie.js
@@ -0,0 +1,19 @@
+const {nodeEnv} = require("../../../config");
+
+const removeAuthCookie = async (req, res) => {
+  try {
+    res
+      .clearCookie("jwt", {
+        maxAge: 0,
+        secure: nodeEnv === "production",
+        sameSite: nodeEnv === "production" ? "none" : "strict",
+      })
+      .send({success: true});
+    console.log("Cookie removed");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({success: false});
+  }
+};
+
+module.exports = removeAuthCookie;
