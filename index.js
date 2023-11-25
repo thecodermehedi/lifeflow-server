98bc0ab5af1855cdd4063cf4bcca01b2bbd95f14
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-26 03:02:10 +0600

    Add server setup and error handling

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require("./src/app");
+const {port} = require("./src/config");
+const connectDB = require("./src/db/connectDB");
+const handleAllErrors = require("./src/utils/error");
+
+// handling all unhandled routes
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
+});
+
+app.use(handleAllErrors);
+
+const runServer = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+};
+
+runServer();
