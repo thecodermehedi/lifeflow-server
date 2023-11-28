292dcd10b724fd6117f44010a66fbef1234e29e3
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:32:14 +0600

    Refactor getUsers function to filter users by role

--- src/api/user/controllers/getUsers.js
+++ src/api/user/controllers/getUsers.js
@@ -3,13 +3,16 @@ const User = require("../../../models/User");
 // Get users
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    let query = {};
+    if (req.query.role) {
+      query.role = req.query.role;
+    }
+    const users = await User.find(query);
     res.send(users);
   } catch (error) {
     console.error(error);
     res.status(503).send({message: "Users Unavailable"});
   }
 };
-
 
 module.exports = getUsers;
