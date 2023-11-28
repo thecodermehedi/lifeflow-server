1652f06e3f5255fb0b035e3f418ff3f4d1adab4a
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:31:43 +0600

    Add user retrieval and update endpoints

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,8 +1,10 @@
 const router = require("../../utils/router");
 
-const {saveUser, getUsers} = require("../../api/user");
+const {saveUser, getUsers, getUser, updateUser} = require("../../api/user");
 
 router.post("/api/v1/users", saveUser);
+router.get("/api/v1/user/:email", getUser);
 router.get("/api/v1/users", getUsers);
+router.patch("/api/v1/user/:email", updateUser);
 
 module.exports = router;
