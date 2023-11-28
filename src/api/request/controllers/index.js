ebd790d4dcd7f32c3eddc5502b61d63d17fcabe6
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:31:18 +0600

    Add request controllers

---
+++ src/api/request/controllers/index.js
@@ -0,0 +1,12 @@
+const saveRequest = require("./saveRequest");
+const getRequests = require("./getRequests");
+const updateReqStatus = require("./updateReqStatus");
+const deleteRequest = require("./deleteRequest");
+const updateRequest = require("./updateRequest");
+module.exports = {
+  saveRequest,
+  getRequests,
+  updateReqStatus,
+  deleteRequest,
+  updateRequest
+};
