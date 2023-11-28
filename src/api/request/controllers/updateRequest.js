b20b8b9de526f4a7cfefb5664fd351a0cad50c3e
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:32:02 +0600

    Add updateRequest controller

---
+++ src/api/request/controllers/updateRequest.js
@@ -0,0 +1,17 @@
+const Request = require("../../../models/Request");
+
+const updateRequest = async (req, res) => {
+  const {id} = req.params;
+  const newRequestInfo = req.body;
+  console.log(newRequestInfo);
+  try {
+    const result = await Request.updateOne({_id: id}, newRequestInfo);
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(503).send({message: "Request Unavailable"});
+  }
+};
+
+module.exports = updateRequest;
