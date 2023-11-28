9131ad8e3a00a35c88ba7682019b3f014c44c62f
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:30:46 +0600

    Add Request model

---
+++ src/models/Request.js
@@ -0,0 +1,19 @@
+const {model, Schema} = require("../utils/mongoose");
+
+const requestSchema = new Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  recipent: {type: String, required: true},
+  hospital: {type: String, required: true},
+  district: {type: String, required: true},
+  upazila: {type: String, required: true},
+  address: {type: String, required: true},
+  message: {type: String, required: true},
+  date: {type: String, required: true},
+  time: {type: String, required: true},
+  dateTime: {type: Date, required: true},
+  status: {type: String, required: true},
+});
+
+const Request = model("Request", requestSchema);
+module.exports = Request;
