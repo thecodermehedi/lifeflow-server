42566c8c577250c0a46b5f52419605f772b7ab4b
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:31:27 +0600

    Add getUser and updateUser controllers

--- src/api/user/controllers/index.js
+++ src/api/user/controllers/index.js
@@ -1,7 +1,10 @@
 const saveUser = require("./saveUser");
 const getUsers = require("./getUsers");
-
+const getUser = require("./getUser");
+const updateUser = require("./updateUser");
 module.exports = {
   saveUser,
   getUsers,
+  getUser,
+  updateUser
 };
