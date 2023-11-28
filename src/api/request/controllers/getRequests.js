32261bfb8687ab1266ff109b71cafb78c9e86610
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:31:10 +0600

    Add getRequests controller for retrieving requests

---
+++ src/api/request/controllers/getRequests.js
@@ -0,0 +1,18 @@
+const Request = require("../../../models/Request");
+
+const getRequests = async (req, res) => {
+  try {
+    let query = {};
+    const userMail = req?.user?.email;
+    if (userMail) {
+      query.email = userMail;
+    }
+    const result = await Request.find(query).sort({ dateTime: -1 });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(503).send({message: "Requests Unavailable"});
+  }
+};
+
+module.exports = getRequests;
