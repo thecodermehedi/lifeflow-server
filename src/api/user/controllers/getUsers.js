74b0c2532b76a2d2b1a5871928716f764a674fc8
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:38:15 +0600

    Refactor getUsers controller to validate and filter users by role

--- src/api/user/controllers/getUsers.js
+++ src/api/user/controllers/getUsers.js
@@ -4,7 +4,8 @@ const User = require("../../../models/User");
 const getUsers = async (req, res) => {
   try {
     let query = {};
-    if (req.query.role) {
+    const validRoles = ["donor", "volunteer", "admin"];
+    if (req.query.role && validRoles.includes(req.query.role)) {
       query.role = req.query.role;
     }
     const users = await User.find(query);
