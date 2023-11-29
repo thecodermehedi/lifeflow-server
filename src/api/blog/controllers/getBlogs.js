b633dd3bf94eb5a0f3ce26f0f3f4a9c6753fe359
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:37:21 +0600

    Add getBlogs controller

---
+++ src/api/blog/controllers/getBlogs.js
@@ -0,0 +1,12 @@
+const Blog = require("../../../models/Blog");
+
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
+
+module.exports = getBlogs;
