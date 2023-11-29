8419aba04a2510f7163e43016ab2a8296295bfb7
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:37:46 +0600

    Add new routes for user API

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,10 +1,21 @@
 const router = require("../../utils/router");
 
-const {saveUser, getUsers, getUser, updateUser} = require("../../api/user");
+const {
+  saveUser,
+  getUsers,
+  getUser,
+  updateUser,
+  updateUserStatus,
+  updateUserRole,
+  getTotalUsers
+} = require("../../api/user");
 
+router.get("/api/v1/users", getUsers);
+router.get("/api/v1/users/total", getTotalUsers);
 router.post("/api/v1/users", saveUser);
 router.get("/api/v1/user/:email", getUser);
-router.get("/api/v1/users", getUsers);
 router.patch("/api/v1/user/:email", updateUser);
+router.patch("/api/v1/user/status/:id", updateUserStatus);
+router.patch("/api/v1/user/role/:id", updateUserRole);
 
 module.exports = router;
