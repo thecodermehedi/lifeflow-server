181488c41d0f394605bcf3d9f64c4ce75eb0c76f
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:38:08 +0600

    Add getTotalUsers controller

---
+++ src/api/user/controllers/getTotalUsers.js
@@ -0,0 +1,14 @@
+const User = require("../../../models/User");
+
+const getTotalUsers = async (req, res) => {
+  try {
+    const result = await User.estimatedDocumentCount();
+    console.log(result);
+    res.status(200).send({total: result});
+  } catch (error) {
+    console.log(error);
+    res.status(503).send({message: "Users Unavailable"});
+  }
+};
+
+module.exports = getTotalUsers;
