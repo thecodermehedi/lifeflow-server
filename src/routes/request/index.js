29e0ee5254e1e010986249752277bcfe907b4e57
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:37:51 +0600

    Add getTotalRequests endpoint

--- src/routes/request/index.js
+++ src/routes/request/index.js
@@ -6,12 +6,13 @@ const {
   updateReqStatus,
   deleteRequest,
   updateRequest,
+  getTotalRequests,
 } = require("../../api/request");
 
-router.post("/api/v1/requests", saveRequest);
+router.get("/api/v1/requests/total", getTotalRequests);
 router.get("/api/v1/requests/:email?", getRequests);
+router.post("/api/v1/requests", saveRequest);
 router.patch("/api/v1/request/status/:id", updateReqStatus);
 router.delete("/api/v1/request/:id", deleteRequest);
 router.patch("/api/v1/request/:id", updateRequest);
-
 module.exports = router;
