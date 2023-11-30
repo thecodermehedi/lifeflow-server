adcbba9cc9a279c787c0527179910351c3f52c74
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 10:26:50 +0600

    Refactor getUsers function to remove role filter

--- src/api/user/controllers/getUsers.js
+++ src/api/user/controllers/getUsers.js
@@ -3,12 +3,7 @@ const User = require("../../../models/User");
 // Get users
 const getUsers = async (req, res) => {
   try {
-    let query = {};
-    const validRoles = ["donor", "volunteer", "admin"];
-    if (req.query.role && validRoles.includes(req.query.role)) {
-      query.role = req.query.role;
-    }
-    const users = await User.find(query);
+    const users = await User.find();
     res.send(users);
   } catch (error) {
     console.error(error);
