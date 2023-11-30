44afd9263d36edc4f940cc54c8b1526c739b45bd
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 10:26:35 +0600

    Add blog controllers for CRUD operations

--- src/api/blog/controllers/index.js
+++ src/api/blog/controllers/index.js
@@ -1,5 +1,13 @@
 const getBlogs = require("./getBlogs");
+const addBlog = require("./addBlog");
+const deleteBlog = require("./deleteBlog");
+const updateBlogStatus = require("./updateBlogStatus");
+const updateBlog = require("./updateBlog");
 
 module.exports = {
   getBlogs,
+  addBlog,
+  deleteBlog,
+  updateBlogStatus,
+  updateBlog
 };
