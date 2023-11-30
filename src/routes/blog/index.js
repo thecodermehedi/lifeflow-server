3b540e22cf70a78496f28940cd2461db9288c0ee
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 10:27:05 +0600

    Add blog routes

--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -1,8 +1,11 @@
 const router = require("../../utils/router");
 
-const {getBlogs} = require("../../api/blog");
-
+const {getBlogs, addBlog, deleteBlog, updateBlogStatus, updateBlog} = require("../../api/blog");
 
 router.get("/api/v1/blogs", getBlogs);
+router.post("/api/v1/blogs", addBlog);
+router.delete("/api/v1/blog/:id", deleteBlog);
+router.patch("/api/v1/blog/status/:id", updateBlogStatus);
+router.patch("/api/v1/blog/:id", updateBlog);
 
 module.exports = router;
