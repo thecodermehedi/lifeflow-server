e93d53f95edc7cefcbed497ac16a5d6a60b923c0
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 20:10:55 +0600

    Add funding controllers

---
+++ src/api/funding/controllers/index.js
@@ -0,0 +1,9 @@
+const paymentIntent = require("./paymentIntent");
+const addPayment = require("./addPayment");
+const getFundingAmmount = require("./getFundingAmmount");
+
+module.exports = {
+  paymentIntent,
+  addPayment,
+  getFundingAmmount,
+};
