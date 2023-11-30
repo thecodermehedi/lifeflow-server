436535d5bb903b28ed557d5253429b19746dc56b
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 17:18:34 +0600

    Add endpoint to get requests by status

--- src/routes/request/index.js
+++ src/routes/request/index.js
@@ -7,12 +7,15 @@ const {
   deleteRequest,
   updateRequest,
   getTotalRequests,
+  getRequestsByStatus
 } = require("../../api/request");
 
 router.get("/api/v1/requests/total", getTotalRequests);
 router.get("/api/v1/requests/:email?", getRequests);
+router.get("/api/v1/request/pending", getRequestsByStatus)
 router.post("/api/v1/requests", saveRequest);
 router.patch("/api/v1/request/status/:id", updateReqStatus);
 router.delete("/api/v1/request/:id", deleteRequest);
 router.patch("/api/v1/request/:id", updateRequest);
+
 module.exports = router;
