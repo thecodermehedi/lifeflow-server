6d5a0ba30601b1681fab3b3632eb804251df5b39
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 20:11:49 +0600

    Update request controller

--- src/api/request/controllers/updateRequest.js
+++ src/api/request/controllers/updateRequest.js
@@ -3,10 +3,8 @@ const Request = require("../../../models/Request");
 const updateRequest = async (req, res) => {
   const {id} = req.params;
   const newRequestInfo = req.body;
-  console.log(newRequestInfo);
   try {
     const result = await Request.updateOne({_id: id}, newRequestInfo);
-    console.log(result);
     res.send(result);
   } catch (error) {
     console.log(error);
